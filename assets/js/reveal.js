522b0ef63458e8da41cea1dc23cd37603b3c9a9d
Marcos Vinicius <marcosviniciuspro4@gmail.com>
2024-01-12 18:20:39 -0300

    Versão final

---
+++ assets/js/reveal.js
@@ -0,0 +1,43 @@
+window.revelar = ScrollReveal({reset:true})
+
+//Topo do site
+revelar.reveal('.efeito-txt-topo', {
+    duration:2000, 
+    distance: '90px'
+})
+revelar.reveal('.botao-topo', {
+    duration:2000, 
+    distance: '90px',
+    delay: 500
+})
+//TITULOS
+
+revelar.reveal('.tituloh2', {
+    duration:2000, 
+    distance: '90px'
+})
+
+
+
+
+
+//CARDS
+revelar.reveal('.primeiro-card', {
+    duration:2000, 
+    distance: '90px',
+    delay:500
+})
+revelar.reveal('.segundo-card', {
+    duration:2000, 
+    distance: '90px',
+    delay:1000
+})
+revelar.reveal('.terceiro-card', {
+    duration:2000, 
+    distance: '90px',
+    delay:1500
+})
+
+
+//Maquinas
+
